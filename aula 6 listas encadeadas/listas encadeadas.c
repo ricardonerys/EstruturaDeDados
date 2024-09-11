7bee3a568886b4d2340b45e136ad76ccdcad5e59
ricardonerys <150439161+ricardonerys@users.noreply.github.com>
2024-09-11 00:31:25 +0000

    atividade listas 10_09

--- aula 6 listas encadeadas/listas encadeadas.c
+++ aula 6 listas encadeadas/listas encadeadas.c
@@ -25,14 +25,20 @@ int main(){
     lista1.head = NULL;
     lista1.tamanho = 0;
     inseririnicio(&lista1,10);
-
+    inseririnicio(&lista1,20);
+    inseririnicio(&lista1,30);
+    inseririnicio(&lista1,40);
+    inserirfim(&lista1,5);
+    inserirfim(&lista1,4);
     
+    imprimirLista(&lista1);
+
     return 0;
 }
 
 
-void inseririnicio(le* l,int val){
-   
+void inseririnicio(le *l,int val){
+    printf("inserindo no inicio:\n");
     no *novono=(no*)malloc(sizeof(no));
     novono->valor = val;
     novono->proximoNo = l->head;
@@ -40,3 +46,37 @@ void inseririnicio(le* l,int val){
     
 
 }
+
+void inserirfim(le *l,int val){
+    printf("inserindo no fim:\n");
+    no *novono=(no*)malloc(sizeof(no));
+
+    no *nop=l->head;
+    while(nop->proximoNo!=NULL){
+        nop=nop->proximoNo;
+    }
+    nop->proximoNo=novono;
+
+    novono->valor = val;
+    novono->proximoNo = NULL;
+
+}
+
+int imprimirLista(le *l){
+    printf("imprimindo valor\n" );
+    if(l->head==NULL){
+        printf("nenhum no na lista\n");
+        return 0;
+    }else{
+        no *nop=l->head;
+        while(nop->proximoNo != NULL){
+        printf("%d\n",nop->valor);
+        nop = nop->proximoNo;
+
+        }
+        printf("%d\n",nop->valor);
+            
+        return 1;
+    }
+}
+    
